418b4f5bbf9d1ac23021bf8cb9e29988a542c645
Africa Nerd <34450905+africanerd@users.noreply.github.com>
2019-01-09 21:27:34 -0500

    Add files via upload

--- africanerdproject1/settings.py
+++ africanerdproject1/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'add  your secret key'
+SECRET_KEY = '1q$ax1z$wli8l(9_u-jwz*(i%qmiixu+vkw5ea7k*bfj4ecylo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'africanerdproject1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
